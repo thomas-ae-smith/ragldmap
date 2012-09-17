e327e3efc9bd5a6215d710ae20598a638205213c
taes1g09 <thomas.ae.smith@googlemail.com>
2012-09-17 14:25:46 +0100

    Added all the other colours of marker. Mid-refactor.

--- RAGLD.js
+++ RAGLD.js
@@ -17,13 +17,134 @@ $('head').append('<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-
 	'<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.4.3/leaflet.ie.css" />' +
 	'<![endif]-->');
 
+var root = this;
+
 // load and execute the vanilla leaflet script
 $.getScript("http://cdn.leafletjs.com/leaflet-0.4.3/leaflet.js", function() {
 // pass JQuery into the $ variable for convenience
 (function( root, $ ){
 	var methods = {};
-	var sources = [];
-	var map = {}; 		//TODO this restricts us to a single map
+	var RAGLDsources = [];
+	var RAGLDmaps = [];
+	var markers = (function() {
+		var markers = {};
+		markers.colours = ['blue', 'green', 'red', 'yellow', 'purple'];
+		markers.auto = 0;
+		var RAGLDMarker = L.Icon.extend({
+			options: {
+				shadowUrl: 'assets/images/marker-shadow.png',
+				iconSize:		[25, 41],
+				shadowSize:		[41, 41],
+				iconAnchor:		[13, 41],
+				// shadowAnchor:	[4, 62],
+				popupAnchor:	[0, -33]
+			}
+		});
+		$.each(markers.colours, function(index, value) {
+			markers[value] = new RAGLDMarker({iconUrl: 'assets/images/marker-icon-' + value + '.png'});
+		})
+		return markers;
+	})();
+
+	function Map(id, options) {
+
+		// set defaults
+		var settings = $.extend( {
+			'geoLocation'			: false,
+			'cloudMade'				: false,
+			'center'				: [50.936592, -1.398697],
+			'zoom'					: 13,
+
+			'APIkey'				: '285675b50972436798d67ce55ab7ddde'	//TODO: remove. Included for testing purposes
+		}, options);
+
+		if (settings['cloudMade'] && 'APIkey' in settings) {
+			settings = $.extend( {
+					'tileString'		: 'http://{s}.tile.cloudmade.com/' + settings.APIkey + '/997/256/{z}/{x}/{y}.png',	//TODO: don't clobber map styles
+					'attribution'		: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+				}, settings);
+		} else {
+			settings = $.extend( {
+				'tileString'		: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+				'attribution'		: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a>'
+			}, settings);
+		}
+
+		map = L.map( id, settings);		//L.map() creates a new object. 
+		this.prototype = L.Map.prototype;
+		
+		RAGLDmaps.push(map);
+
+		map.mapSources = [];
+
+		this.prototype.addSource = function (sourceURI) {
+			var map = this;
+			$.when(methods.dataSource(sourceURI)).then( function(newSource) {
+				console.log("Data received:", newSource.data);
+				var label = "", lat = "", lng = "", invalid = 0;
+				console.log("Colour is " + newSource.colour + " and marker is ", markers[newSource.colour]); //TODO use of newSource is confusing with source
+				$.each(newSource.data, function(key, value) {
+					try {
+						lat = value['http://www.w3.org/2003/01/geo/wgs84_pos#lat'][0].value;
+						lng = value['http://www.w3.org/2003/01/geo/wgs84_pos#long'][0].value;
+						label = (value['http://www.w3.org/2000/01/rdf-schema#label'])? value['http://www.w3.org/2000/01/rdf-schema#label'][0].value : key.slice(key.lastIndexOf("/", key.lastIndexOf("/")-1));
+							// console.log("Setting marker for " + label + " at [" + lat + "," + lng + "].");
+							L.marker([lat, lng], {icon: markers[newSource.colour]}).addTo(map).bindPopup("<a href=\"" + key + "\">" + label + "<\a>");
+						} catch (error) {
+							invalid += 1;
+						}
+					});
+
+				if (invalid) {
+					$('<div class="warning"><p class="text-warning">Warning: Found ' + invalid + " unusable items.</p></div>").insertAfter('.map');
+				}
+			});
+
+		};
+		this.prototype.clearSources = function () {
+
+		};
+
+
+		L.tileLayer( settings.tileString, {
+			attribution: settings.attribution,
+		}).addTo(map);
+
+
+		if ('source' in settings) {
+			methods.showNewPoints(map, settings.source);
+		}
+
+		if ('sources' in settings) {
+			console.log("sources", settings.sources);
+			$.each(settings.sources, function(index, value) {
+				// methods.showNewPoints(map, value)
+				map.addSource(value);
+			});
+		}
+
+		if (settings['geoLocation']) {
+			try {
+				navigator.geolocation.getCurrentPosition( function(position) {
+					console.log("Current Geolocation:" + position);
+					map.setView([position.coords.latitude, position.coords.longitude], settings.zoom);
+					L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
+					.bindPopup('Current<br>geo-location.').openPopup();
+				})
+			} catch (error) {
+				console.log("Geolocation failed, remained at default position.");	//TODO: sensible defaults. maybe
+			}
+		} //if geoLocation
+
+		return map;
+	}
+
+	this.map = function (id, options) {
+		return Map(id, options);
+	};
+
+	console.log("GotScript. This is ", this, " root is ", root);
+	console.log("markers are ", markers);
 
 	$.fn.RAGLD = function( method ) {
 
@@ -39,78 +160,32 @@ $.getScript("http://cdn.leafletjs.com/leaflet-0.4.3/leaflet.js", function() {
 
 	methods = {
 		createMap : function( options ) {
-			console.log("should locate:", options['geoLocation']);
-			// set defaults
-			var settings = $.extend( {
-				'geoLocation'			: false,
-				'cloudMade'				: false,
-				'center'				: [50.936592, -1.398697],
-				'zoom'					: 15,
+			map = map( this[0], options);
+			return;
+		},	//createMap
 
-				'APIkey'				: '285675b50972436798d67ce55ab7ddde'	//TODO: remove. Included for testing purposes
-			}, options);
-
-			if (settings['cloudMade'] && 'APIkey' in settings) {
-				settings = $.extend( {
-					'tileString'		: 'http://{s}.tile.cloudmade.com/' + settings.APIkey + '/997/256/{z}/{x}/{y}.png',	//TODO: don't clobber map styles
-					'attribution'		: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-				}, settings);
-			} else {
-				settings = $.extend( {
-					'tileString'		: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-					'attribution'		: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a>'
-				}, settings);
-			}
-
-			return this.each(function() {
-				//at the moment the plugin calls getscript again before each creation
-				var this_ = this;	//sadly necessary due to above
-				$.getScript("http://cdn.leafletjs.com/leaflet-0.4.3/leaflet.js", function(){
-					map = L.map( this_, settings);
-					L.tileLayer( settings.tileString, {
-						attribution: settings.attribution,
-					}).addTo(map);
-
-
-					if ('source' in settings) {
-						methods.showNewPoints(map, settings.source);
-					}
-
-					if ('sources' in settings) {
-						console.log("sources", settings.sources);
-						$.each(settings.sources, function(index, value) {
-							methods.showNewPoints(value)
-						});
-					}
-
-					if (settings['geoLocation']) {
-						try {
-							navigator.geolocation.getCurrentPosition( function(position) {
-								console.log("Current Geolocation:" + position);
-								map.setView([position.coords.latitude, position.coords.longitude], settings.zoom);
-								L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
-								.bindPopup('Current<br>geo-location.').openPopup();
-							})
-						} catch (error) {
-							console.log("Geolocation failed, remained at default position.");	//TODO: sensible defaults. maybe
-						}
-					} //if geoLocation
-				});	//getscript
-			});	//each
-		},	//initMap
-
-		dataSource : function( source, colour ) {
+		dataSource : function( sourceURI, colour ) {
 			var deferred = $.Deferred();
 
-			console.log("Adding " + colour + " source data from ", source);
 			var newSource = {};
-			newSource.source = source;
-			newSource.colour = colour;
+			newSource.source = sourceURI;
+
+			newSource.colour = (function(colour) {
+				if ($.inArray(colour, markers.colours) >= 0) {
+					return colour;
+				}
+				//TODO: candidate for map reduce, but not familar enough with it yet
+				//this works for the moment, sorta.
+				markers.auto += 1;
+				return markers.colours[markers.auto-1];
+			})(colour);
+			console.log("Adding " + newSource.colour + " source data from " + sourceURI);
+
 			try {
-				$.getJSON(source, function(data) {
+				$.getJSON(sourceURI, function(data) {
 					console.log("Data retrieved:", data);
 					newSource.data = data;
-					sources.push(newSource);
+					RAGLDsources.push(newSource);
 					deferred.resolve(newSource);
 				});
 			} catch (error) {
@@ -121,18 +196,19 @@ $.getScript("http://cdn.leafletjs.com/leaflet-0.4.3/leaflet.js", function() {
 			return deferred.promise();
 		},	//dataSource
 
-		showNewPoints : function( source ) {
+		showNewPoints : function( map, sourceURI ) {
 			console.log("Map is ", map);
-			$.when(methods.dataSource(source)).then( function(newSource) {
+			$.when(methods.dataSource(sourceURI)).then( function(newSource) {
 				console.log("Data received:", newSource.data);
 				var label = "", lat = "", lng = "", invalid = 0;
+				console.log("Colour is " + newSource.colour + " and marker is ", markers[newSource.colour]); //TODO use of newSource is confusing with source
 				$.each(newSource.data, function(key, value) {
 					try {
 						lat = value['http://www.w3.org/2003/01/geo/wgs84_pos#lat'][0].value;
 						lng = value['http://www.w3.org/2003/01/geo/wgs84_pos#long'][0].value;
 						label = (value['http://www.w3.org/2000/01/rdf-schema#label'])? value['http://www.w3.org/2000/01/rdf-schema#label'][0].value : key.slice(key.lastIndexOf("/", key.lastIndexOf("/")-1));
 							// console.log("Setting marker for " + label + " at [" + lat + "," + lng + "].");
-							L.marker([lat, lng]).addTo(map).bindPopup("<a href=\"" + key + "\">" + label + "<\a>");
+							L.marker([lat, lng], {icon: markers[newSource.colour]}).addTo(map).bindPopup("<a href=\"" + key + "\">" + label + "<\a>");
 						} catch (error) {
 							invalid += 1;
 						}
@@ -146,5 +222,5 @@ $.getScript("http://cdn.leafletjs.com/leaflet-0.4.3/leaflet.js", function() {
 		}	//showPoints
 	};
 
-})( this, jQuery );
+})( root, jQuery );
 });
